5decc5b84d998b551c0fee4573dc13f1a51ccdbe
mrmerarif <bflores2@byupathway.edu>
2026-06-16 03:20:40 -0600

    updating package jso

--- meal-master-pro-api/models/Recipe.js
+++ meal-master-pro-api/models/Recipe.js
@@ -1,11 +1,33 @@
 import mongoose from "mongoose";
 
-const recipeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  ingredients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ingredient" }],
-  steps: [String],
-  createdAt: { type: Date, default: Date.now }
-});
+const recipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    ingredients: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient"
+      }
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    collection: "recipes"
+  }
+);
 
-export default mongoose.model("Recipe", recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
